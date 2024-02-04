0a841a6c4385ec404f2e25a9c7ca064de4160ae2
Ilya Panov <majnun42@gmail.com>
2024-02-04 14:26:14 +0500

    added test case

--- top_c++/homework_14/homework_14.cpp
+++ top_c++/homework_14/homework_14.cpp
@@ -9,7 +9,8 @@ int main()
     const char* str1{""};
     const char* str2{ "Hello world" };
     const char* str3{ "                " };
-    cout << strSize(str1) << "\n" << strSize(str2) << "\n" << strSize(str3) << "\n" << endl;
+    const char* str4{ "Hell\0o unreacheable world" };
+    cout << strSize(str1) << "\n" << strSize(str2) << "\n" << strSize(str3) << "\n" << strSize(str4) << endl;
 }
 
 size_t strSize(const char* str) {
