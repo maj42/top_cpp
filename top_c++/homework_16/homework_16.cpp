984ce348ff897b735b2ac5070a8e1fb11f01411e
Ilya Panov <majnun42@gmail.com>
2024-02-15 22:49:58 +0500

    homework 16

---
+++ top_c++/homework_16/homework_16.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void myMemCpy(void* dest, void* src, size_t count);
+
+int main()
+{
+    char source[] { "Hello World" };
+    char* destination{};
+    void* srcPtr = source;
+    void* dstPtr = destination;
+    myMemCpy(dstPtr, srcPtr, sizeof(source));
+    
+    for (int i = 0; i < sizeof(source); i++) {
+        cout << *((char*)destination + i);
+    } 
+}
+
+void myMemCpy(void* dest, void* src, size_t count) {
+    dest = malloc(count);
+    for (int i = 0; i < count; i++) {
+        *((char*)dest + i) = *((char*)src + i);
+    }
+}
