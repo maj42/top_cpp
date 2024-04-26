8fb838917df9a14df4a5f36d8fdab77cb87eb68c
Ilya Panov <majnun42@gmail.com>
2024-04-26 14:20:34 +0500

    homework 37 done, 36<->37

--- top_c++/homework_36/homework_36.cpp
+++ top_c++/homework_36/homework_36.cpp
@@ -1,45 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 
 using namespace std;
 
-union Var {
-	int signedInt;
-	unsigned unsignedInt;
-};
-
-struct Animal {
-	string specName;
-	string specClass;
-	string name;
-	string voice;
-
-	Animal(string spN, string spCl, string name, string voice = "I can't talk right now") : specName(spN), specClass(spCl), name(name), voice(voice) {}
-
-	void print() {
-		cout << name << ":\n\tSpecies name: " << specName << "\n\tSpecies Class: " << specClass << "\n\n";
-	}
-
-	void talk() {
-		cout << name << " says: " << voice << "\n\n";
-	}
-};
-
-int main()
+void main()
 {
-	//task1 Union
-	Var variable{-10};
-	cout << "Signed: " << variable.signedInt << "\nUnsigned: " << variable.unsignedInt << "\n\n";
-	
-	//task2 Struct
-	Animal cat{ "Felis Catus", "Mammalia", "Vasya", "Meow" };
-	Animal dog{ "Canis lupus familiaris", "Mammalia", "Rex", "Woof" };
-	Animal platypus{ "Ornithorhynchus anatinus", "Mammalia", "Perry" };
+	FILE* file;
+	FILE* newFile;
+	char fileName[] = "words.txt";
+	char newFileName[] = "newWords.txt";
 
-	cat.print();
-	cat.talk();
-	dog.print();
-	dog.talk();
-	platypus.print();
-	platypus.talk();
-
+	if ((file = fopen(fileName, "r")) && (newFile = fopen(newFileName, "w"))) {
+		char word[20];
+		while (!feof(file)) {
+			fscanf(file, "%s", word);
+			if (strlen(word) >= 7) {
+				fprintf(newFile, word);
+				fprintf(newFile, "\n");
+			}
+		}
+		fclose(file);
+		fclose(newFile);
+	}
 }
+
---
+++ top_c++/homework_37/homework_37.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+union Var {
+	int signedInt;
+	unsigned unsignedInt;
+};
+
+struct Animal {
+	string specName;
+	string specClass;
+	string name;
+	string voice;
+
+	Animal(string spN, string spCl, string name, string voice = "I can't talk right now") : specName(spN), specClass(spCl), name(name), voice(voice) {}
+
+	void print() {
+		cout << name << ":\n\tSpecies name: " << specName << "\n\tSpecies Class: " << specClass << "\n\n";
+	}
+
+	void talk() {
+		cout << name << " says: " << voice << "\n\n";
+	}
+};
+
+int main()
+{
+	//task1 Union
+	Var variable{ -10 };
+	cout << "Signed: " << variable.signedInt << "\nUnsigned: " << variable.unsignedInt << "\n\n";
+
+	//task2 Struct
+	Animal cat{ "Felis Catus", "Mammalia", "Vasya", "Meow" };
+	Animal dog{ "Canis lupus familiaris", "Mammalia", "Rex", "Woof" };
+	Animal platypus{ "Ornithorhynchus anatinus", "Mammalia", "Perry" };
+
+	cat.print();
+	cat.talk();
+	dog.print();
+	dog.talk();
+	platypus.print();
+	platypus.talk();
+
+}
